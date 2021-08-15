8d9566d05c46666fd7aba87f29e2f366b352c035
Andrea Konomi <andreakonomi23@gmail.com>
2021-08-15 15:37:36 +0200

    TextFile Connection set

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,17 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
+
         //TODO: Wire up the CreatePrize to text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //Load the text file and convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
 
-            return model;
+            //Find the max ID
+            int currentId = 1;      //if file empty
+
+            if (prizes.Count > 1)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+                //currentId = prizes.Max(p => p.Id) + 1; can use this as well it's better
+            }
+
+            model.Id = currentId;
+
+            //Ade new record with the new maxId + 1
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
+
+            return model; 
+
+            // Load the text file to list<string>>
+            //Convert to List<PrizeModel>
+            //Find the max ID
+            //Add the new record with the new ID
+            //Convert the prizes to a list<string>
+            //Save the list<string> to the text file
         }
     }
 }
---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess.TextHelpers
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(this string fileName) //made it an extension method
+        {
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{fileName}";
+        }
+
+        public static List<string> LoadFile(this string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(filePath).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach (string l in lines)
+            {
+                string[] cols = l.Split(',');
+
+                PrizeModel p = new PrizeModel();
+                p.Id = int.Parse(cols[0]);      //crash if this not ok
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[4]);
+
+                output.Add(p);
+            }
+
+            return output;
+        }
+
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new();
+
+            foreach (PrizeModel p in models)
+            {
+                lines.Add($"{p.Id},{p.PlaceNumber},{p.PlaceName},{p.PrizeAmount},{p.PrizePercentage}");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+    }
+}
