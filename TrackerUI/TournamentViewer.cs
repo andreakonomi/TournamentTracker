248ccdeb0ff0875125d18ee6a1c3cb29777f8e2d
Andrea Konomi <andreakonomi23@gmail.com>
2021-09-11 19:14:05 +0200

    Error Handling Lesson done

--- TrackerUI/TournamentViewer.cs
+++ TrackerUI/TournamentViewer.cs
@@ -169,8 +169,44 @@ namespace TrackerUI
             LoadMatchup();
         }
 
+        private string ValidateData()
+        {
+            string output = "";
+            double teamOneScore = 0;
+            double teamTwoScore = 0;
+
+            bool scoreOneValid = double.TryParse(txtTeamOneScoreValue.Text, out teamOneScore);
+            bool scoreTwoValid = double.TryParse(txtTeamTwoScore.Text, out teamTwoScore);
+            
+            if (!scoreOneValid)
+            {
+                output = "The Score One value is not a valid number.";
+            }
+            else if (!scoreTwoValid)
+            {
+                output = "The Score Two value is not a valid number.";
+            }
+            else if (teamOneScore == 0 || teamTwoScore == 0)
+            {
+                output = "You did not enter a score for either team.";
+            }
+            else if (teamOneScore == teamTwoScore)
+            {
+                output = "We do not allow ties for this application.";
+            }
+
+            return output;
+        }
+
         private void btnScore_Click(object sender, EventArgs e)
         {
+            string errorMessage = ValidateData();
+            if (errorMessage.Length > 0)
+            {
+                MessageBox.Show($"Input error: {errorMessage}");
+                return;
+            }
+
             double teamOneScore = 0;
             double teamTwoScore = 0;
             if (selectedMatchups.Count == 0) return;
@@ -211,7 +247,15 @@ namespace TrackerUI
                 }
             }
 
-            TournamentLogic.UpdateTournamentResults(tournament);
+            try
+            {
+                TournamentLogic.UpdateTournamentResults(tournament);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The application had the following error: {ex.Message} ");
+                return;
+            }
 
             RefreshControlsData();
 
